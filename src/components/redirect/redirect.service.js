0bfd01725534c6541c6cb788b3f057cbf46b7753
axelluza <axel.luzav@gmail.com>
2023-09-09 18:58:09 -0500

    redirect solo get

--- src/components/redirect/redirect.service.js
+++ src/components/redirect/redirect.service.js
@@ -3,13 +3,17 @@ import {
 	getUrlRedirectDao,
 } from "./redirect.dao.js";
 import {
-	UrlNotFoundError,
-} from "../errors.js";
+	validateGetRedirect,
+} from "./redirect.validation.js";
 
 export const getUrlRedirect = async (url) => {
+	const verified = validateGetRedirect({ url })
+	if(verified.valid==false){
+		return "https://winwinafi.com";
+	}
 	const result = await getUrlRedirectDao(url);
 	if(!result){
-		throw UrlNotFoundError;
+		return "https://winwinafi.com";
 	}
 	return result?.destinationUrl;
 };
--- src/components/redirect/redirect.validation.js
+++ src/components/redirect/redirect.validation.js
@@ -5,8 +5,21 @@ export const redirectSchema = Joi.object().keys({
   destinationUrl: Joi.string().required(),
 });
 
+export const getRedirectSchema = Joi.object().keys({
+  url: Joi.string().required().min(8).max(10).alphanum(),
+});
+
 export const validateRedirect = (data) => {
   const { error } = redirectSchema.validate(data, { abortEarly: false });
+  if (error) {
+    const messages = error.details.map((detail) => detail.message);
+    return { valid: false, errors: messages };
+  }
+  return { valid: true };
+};
+
+export const validateGetRedirect = (data) => {
+  const { error } = getRedirectSchema.validate(data, { abortEarly: false });
   if (error) {
     const messages = error.details.map((detail) => detail.message);
     return { valid: false, errors: messages };
