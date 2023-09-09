b188d3094991603b911389e572a9814de8b653fe
axelluza <axel.luzav@gmail.com>
2023-09-09 16:25:27 -0500

    redirect solo get

--- src/components/redirect/redirect.dao.js
+++ src/components/redirect/redirect.dao.js
@@ -1,6 +1,6 @@
 import { urlShortener } from "./redirect.model.js";
 
 export const getUrlRedirectDao = async (url) => {
-	const result = await urlShortener.findOne({shortUrl:url}).lean();
+	const result = await urlShortener.findOne({shortUrl:url,status:"active"}).lean();
 	return result;
 };
