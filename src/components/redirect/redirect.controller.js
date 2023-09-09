96602daa475d74c1ce6f9e658047c9d2053fd075
axelluza <axel.luzav@gmail.com>
2023-09-09 16:14:43 -0500

    redirect solo get

--- src/components/redirect/redirect.controller.js
+++ src/components/redirect/redirect.controller.js
@@ -4,7 +4,7 @@ export const getUrlRedirectHandler = async (req, res, next) => {
   try {
     const url = req.params.url;
     const getUrl = await getUrlRedirect(url);
-    res.status(200).json({ data: getUrl });
+    res.redirect(getUrl);
   } catch (err) {
     next(err);
   }
