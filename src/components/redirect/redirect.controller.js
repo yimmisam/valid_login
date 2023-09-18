f15652787e784b4998d0f428b8c545e8071ef489
axelluza <axel.luzav@gmail.com>
2023-09-18 00:24:50 -0500

    add query.wwtoken

--- src/components/redirect/redirect.controller.js
+++ src/components/redirect/redirect.controller.js
@@ -3,7 +3,10 @@ import { getUrlRedirect } from "./redirect.service.js";
 export const getUrlRedirectHandler = async (req, res, next) => {
   try {
     const url = req.params.url;
-    const getUrl = await getUrlRedirect(url);
+    const getUrl = await getUrlRedirect(url);    
+    if(req.query.wwtoken){
+      getUrl=getUrl+"?wwtoken="+req.query.wwtoken
+    }
     res.redirect(getUrl);
   } catch (err) {
     next(err);
